465e19cf95b37bf7c4a53dca046203ae0698da6d
sara <sarammartinsoliveira@gmail.com>
2026-05-06 13:23:31 -0300

    Estudando comandos SQL com DBeaver

---
+++ bd_programaria/insert_tabelagerencia.sql
@@ -0,0 +1,10 @@
+INSERT INTO Gerencia_Regiao (Regiao, 
+Pessoas_brancas, 
+Pessoas_pretas_pardas, 
+Gerencia_branca, 
+Gerencia_preta_parda) VALUES
+('Norte', 1480, 6367, 43, 78),
+('Nordeste', 5418, 16211, 146, 179),
+('Sudeste', 22265, 20783, 928, 371 ),
+('Sul', 11298, 3945, 421, 49),
+('Centro-Oeste', 2888, 5274, 104, 95 );
